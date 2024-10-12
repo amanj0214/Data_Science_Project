6cfe751ee3956d98e223fc8784a461c3f5cd14b1
amanfj <jiddewar.amandeep@gmail.com>
2024-10-12 00:03:42 -0400

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template, jsonify
-import src.model_repository as model_repository
+# import src.model_repository as model_repository
 import pandas as pd
 
 app = Flask(__name__)
 
 # Load model using the abstracted load_model function
-model = model_repository.load_model("titanic_survival_model")
+# model = model_repository.load_model("titanic_survival_model")
 
 @app.route('/')
 def home():
@@ -13,8 +13,10 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    return jsonify({"prediction": 45})
     # Assume that form data contains the input features in JSON
     data = request.get_json(force=True)
+    return jsonify({"prediction": 45})
     df = pd.DataFrame([data])
     
     # Make predictions using the loaded model
@@ -24,4 +26,4 @@ def predict():
     return jsonify({"prediction": int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
