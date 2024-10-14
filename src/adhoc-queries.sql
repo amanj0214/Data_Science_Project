29b6f42137c73722aa5543a51e6760c1efa9e089
amanfj <jiddewar.amandeep@gmail.com>
2024-10-14 00:18:50 -0400

    update requirements.txt to able to handle windows packages.

---
+++ src/adhoc-queries.sql
@@ -0,0 +1,11 @@
+-- Get all the returned categories
+select i_category, i.i_class, count(1)
+from web_returns wr join item i on wr.wr_item_sk = i.i_item_sk
+group by i_category, i_class
+
+
+-- Get all the returns for Electronics
+select *
+from web_returns wr join item i on wr.wr_item_sk = i.i_item_sk
+WHERE i.i_category = 'Electronics'
+limit 10
